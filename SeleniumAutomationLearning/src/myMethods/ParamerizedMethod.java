9398bbbac22b547f8f97612632625984a76b752e
chetan velhal <chetan.velhal@vanderlande.com>
2023-08-14 12:26:14 +0530

    Create any paramerized method

---
+++ SeleniumAutomationLearning/src/myMethods/ParamerizedMethod.java
@@ -0,0 +1,17 @@
+package myMethods;
+
+public class ParamerizedMethod {
+
+	public static void NameAge(String name, int age)
+	{
+		System.out.println(name + age );
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		ParamerizedMethod.NameAge("chetan", 35);
+	}
+
+}
