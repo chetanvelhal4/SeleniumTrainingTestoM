f171c6fd0e82f1044451118e4bc9044ab5eaeadf
chetan velhal <chetan.velhal@vanderlande.com>
2023-09-07 12:34:20 +0530

    Output of below Code

---
+++ SeleniumAutomationLearning/src/myMethods/ABC.java
@@ -0,0 +1,15 @@
+package myMethods;
+
+public class ABC {
+	private static int x;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(fun());
+	}
+
+	static int fun() {
+		return ++x;
+	}
+
+}
