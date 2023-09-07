2c7333eeb6ae6b0613374462743e0fce4aa3abd7
chetan velhal <chetan.velhal@vanderlande.com>
2023-09-07 16:33:55 +0530

    Constructor as private access modifier and create object for object

---
+++ SeleniumAutomationLearning/src/constructor/ConstructorOne.java
@@ -0,0 +1,18 @@
+package constructor;
+
+class A {
+
+	public A() {
+
+	}
+
+}
+
+public class ConstructorOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		A a1 = new A();
+	}
+
+}
