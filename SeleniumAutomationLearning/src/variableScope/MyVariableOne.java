6855bf01125a560398c74e0f40ee067eb27a1556
chetan velhal <chetan.velhal@vanderlande.com>
2023-09-08 10:47:03 +0530

    Write Output

---
+++ SeleniumAutomationLearning/src/variableScope/MyVariableOne.java
@@ -0,0 +1,14 @@
+package variableScope;
+
+public class MyVariableOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(fun());
+	}
+
+	static int fun() {
+		return 20;
+	}
+
+}
